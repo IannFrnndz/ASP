e9b510ed79ec2961c15a43c680ba73977f6cb4f6
Ian Fernández Gamo <ian.fernandezgamo24@campusfp.es>
2025-10-30 13:43:36 +0100

    debugging

---
+++ EjemploDebuging/EjemploDebuging/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace EjemploDebugging
+{
+    internal class Program
+    {
+
+        static void Hola()
+        {
+            Console.WriteLine("Hola buenas tardes ");
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hola buenas tardes ");
+            Hola();
+
+            string patata = Console.ReadLine();
+
+            int valor = Convert.ToInt32(patata);
+
+            for(int i = 0; i < valor; i++)
+            {
+                Console.WriteLine($"El valor de i es: {i}");
+            }
+        }
+    }
+}
