c408ed51c4f79a57fa4ca65a191a0fb3b26f7f83
Ian <iannomore5@gmail.com>
2025-12-03 23:34:38 +0100

    Ejercicios de Tag Helpers

---
+++ TagHelpers/TagHelpers/Controllers/MovieController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TagHelpers.Models;
+
+
+public class MovieController : Controller
+{
+    // GET: mostrar formulario Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: recibir datos del formulario
+    [HttpPost]
+    public IActionResult Create(Movie model)
+    {
+        if (ModelState.IsValid)
+        {
+            // Aquí normalmente guardarías en base de datos
+            return RedirectToAction("Index");
+        }
+
+        // Si hay errores de validación
+        return View(model);
+    }
+
+    // GET: listado de películas (para probar el href)
+    public IActionResult Index()
+    {
+        // Vista mínima, puede estar vacía
+        return View();
+    }
+}
---
+++ TagHelpers/TagHelpers/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Movie
+{
+    public int ID { get; set; }
+
+    public string Title { get; set; }
+
+    [Required(ErrorMessage = "La fecha de estreno es obligatoria")]
+    public DateTime ReleaseDate { get; set; }
+
+    public decimal Price { get; set; }
+}
